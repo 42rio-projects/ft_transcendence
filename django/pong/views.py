a86fc8cf0d2e8d05901b6da00e44cfc54a331269
Vitor Sergio <vitorsergio111@gmail.com>
2023-12-09 17:27:18 -0300

    feat: menu + opening game at 'local multiplayer'

--- django/pong/views.py
+++ django/pong/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
-def index(request):
-    return render(request, "index.html")
+def menu(request):
+    return render(request, "menu/index.html")
+
+def game(request):
+    return render(request, "game/index.html")
