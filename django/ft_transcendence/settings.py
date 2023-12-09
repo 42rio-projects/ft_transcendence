a3322ddccf09db3f9b0ba4aedbb7661d64a17952
Vitor Sergio <vitorsergio111@gmail.com>
2023-12-09 15:56:08 -0300

    refactor: remove comments

--- django/ft_transcendence/settings.py
+++ django/ft_transcendence/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'templates'),  # Adicione este diretório
+    os.path.join(BASE_DIR, 'templates'),
 ]
 
 # Default primary key field type
