33458abdfb8d4be4311f670a1e38aeac19bde644
gsistelos <gabrielsistelos@gmail.com>
2023-12-13 18:50:55 -0300

    feat: user get/post/delete endpoints

--- django/pong/urls.py
+++ django/pong/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.menu, name="menu"),
     path("game/", views.game, name="game"),
-    path("api/users", views.User.as_view(), name="users"),
+    path("api/users", views.UserEndpoint.as_view(), name="users"),
     path("api/games", views.Game.as_view(), name="games"),
 ]
--- django/pong/views.py
+++ django/pong/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 
 def menu(request):
@@ -11,12 +13,38 @@ def game(request):
     return render(request, "game/index.html")
 
 
-class User(APIView):
+class UserEndpoint(APIView):
     def get(self, request, format=None):
-        return Response({"method": "get", "data": request.data})
+        try:
+            users = User.objects.all()
+            return Response(users.values())
+        except User.DoesNotExist as e:
+            return Response({"error": str(e)}, 404)
+        except Exception as e:
+            return Response({"error": str(e)}, 500)
 
     def post(self, request, format=None):
-        return Response({"method": "post", "data": request.data})
+        try:
+            user = User(username=request.data["username"],
+                        password=request.data["password"])
+            user.save()
+            return Response({"created": user.username})
+        except IntegrityError as e:
+            return Response({"error": str(e)}, 409)
+        except Exception as e:
+            return Response({"error": str(e)}, 500)
+
+    def delete(self, request, format=None):
+        try:
+            user = User.objects.get(
+                username=request.data["username"],
+                password=request.data["password"])
+            user.delete()
+            return Response({"deleted": user.username})
+        except User.DoesNotExist as e:
+            return Response({"error": str(e)}, 404)
+        except Exception as e:
+            return Response({"error": str(e)}, 500)
 
 
 class Game(APIView):
