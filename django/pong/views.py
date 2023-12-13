940eeb51281025efb32d6c977e0beba61360bf03
gsistelos <gabrielsistelos@gmail.com>
2023-12-13 18:25:18 -0300

    feat: simple restful endpoint

--- django/pong/views.py
+++ django/pong/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 
 def menu(request):
     return render(request, "menu/index.html")
 
+
 def game(request):
     return render(request, "game/index.html")
+
+
+class User(APIView):
+    def get(self, request, format=None):
+        return Response({"method": "get", "data": request.data})
+
+    def post(self, request, format=None):
+        return Response({"method": "post", "data": request.data})
+
+
+class Game(APIView):
+    def get(self, request, format=None):
+        return Response({"method": "get", "data": request.data})
+
+    def post(self, request, format=None):
+        return Response({"method": "post", "data": request.data})
