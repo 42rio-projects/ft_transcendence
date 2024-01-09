d90037804d66cad12dc91e73eef707f777f8093a
gabrielclcardoso <gabrielclcardoso@gmail.com>
2024-01-09 18:57:16 -0300

    feat: create tournament model

--- django/pong/models.py
+++ django/pong/models.py
@@ -4,6 +4,11 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class Tournament(models.Model):
+    name = models.CharField(max_length=20)
+    end = models.DateField(auto_now_add=True)
+
+
 class Game(models.Model):
     winner = models.ForeignKey(
         User, null=True, on_delete=models.SET_NULL, related_name='wins')
@@ -12,3 +17,5 @@ class Game(models.Model):
     winner_points = models.PositiveSmallIntegerField()
     loser_points = models.PositiveSmallIntegerField()
     end = models.DateField(auto_now_add=True)
+    tournament = models.ForeignKey(
+        Tournament, null=True, on_delete=models.SET_NULL, related_name="games")
