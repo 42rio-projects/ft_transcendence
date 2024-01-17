603ab09caf8909fbb62babb753926fc4e7398d46
gabrielclcardoso <gabrielclcardoso@gmail.com>
2024-01-17 14:46:55 -0300

    feat: add Message and Chat models

--- django/pong/models.py
+++ django/pong/models.py
@@ -4,6 +4,22 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class Chat(models.Model):
+    name = models.CharField(max_length=20)
+    members = models.ManyToManyField(User)
+
+
+class Message(models.Model):
+    content = models.CharField()
+    date = models.DateTimeField(auto_now_add=True)
+    sender = models.ForeignKey(
+        User, null=True, on_delete=models.SET_NULL, related_name="messages"
+    )
+    chat = models.ForeignKey(
+        Chat, on_delete=models.CASCADE, related_name="messages"
+    )
+
+
 class Tournament(models.Model):
     name = models.CharField(max_length=100, unique=True)
     beginning = models.DateField(auto_now_add=True)
