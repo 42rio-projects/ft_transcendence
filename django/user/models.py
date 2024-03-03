5740ee840d11df9342a81760878b7b40b1103f82
gabrielclcardoso <gabrielclcardoso@gmail.com>
2024-03-03 19:28:39 -0300

    feat: add friends to User model

--- django/user/models.py
+++ django/user/models.py
@@ -1,6 +1,31 @@
-# from django.db import models
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    friends = models.ManyToManyField('self', through="IsFriendsWith")
+
+
+class IsFriendsWith(models.Model):
+    user1 = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='user1'
+    )
+    user2 = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='user2'
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                name="%(app_label)s_%(class)s_unique_relationships",
+                fields=["user1", "user2"]
+            ),
+            models.CheckConstraint(
+                name="%(app_label)s_%(class)s_prevent_self_follow",
+                check=~models.Q(user1=models.F("user2")),
+            ),
+        ]
